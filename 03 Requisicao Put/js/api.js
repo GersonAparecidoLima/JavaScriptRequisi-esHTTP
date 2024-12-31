fb6385eb41051b2392fde32b3c74602a47d0d349
Gerson Aparecido <gerson.lima@yduqs.com.br>
2024-12-31 17:12:54 -0300

    metodo HTTP PUT para alterar dados no servidore

---
+++ 03 Requisicao Put/js/api.js
@@ -0,0 +1,62 @@
+const api = {
+  //Get
+  async buscarPensamentos() {
+    try {
+      const response = await fetch('http://localhost:3000/pensamentos')
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao buscar pensamentos')
+      throw error
+    }
+  },
+
+  async salvarPensamento(pensamento) {
+    try {
+      const response = await fetch('http://localhost:3000/pensamentos', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao salvar pensamento')
+      throw error
+    }
+  },
+
+  //Get
+  async buscarPensamentoPorId(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao buscar pensamento')
+      throw error
+    }
+  },
+
+  async editarPensamento(pensamento) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pensamento)
+      })
+      return await response.json()
+    }
+    catch {
+      alert('Erro ao editar pensamento')
+      throw error
+    }
+  },
+
+}
+
+export default api
